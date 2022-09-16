151735960c3ec40318bfeb8a2ace065640cc9acc
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-09-16 14:46:48 +0300

    start of tragedy

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    
+    }).join('');
+}
+// console.log(markup);
+imageConteiner.addEventListener('click', onClickImage);
+
+function onClickImage(evt) {
+    evt.preventDefault()
+    console.log(evt.target);
+  const instance = 	basicLightbox.create(imageMarkup).show()
+    
+}
