df40f3b958baf1e28dfb0cc2658557c2ef59151c
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-09-17 10:09:37 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -19,21 +19,12 @@ function createGallery(galleryItems) {
 // console.log(markup);
 imageConteiner.addEventListener('click', onClickImage);
 const lightbox = new SimpleLightbox('.gallery a', {
-    // captions: true,
-    // // captionSelector: 'img',
-    // // captionType: 'attr',
     captionsData: 'alt',
     captionDelay: 250
 });
-// SimpleLightbox.default = {
-//     showCaption: true, captionAttribute: 'alt',
-// }
 function onClickImage(evt) {
     evt.preventDefault()
     if (evt.target.nodeName !== "IMG") {
         return;
     }
-    const clickImg = evt.target.dataset.source
-    
-
-}
+  }
