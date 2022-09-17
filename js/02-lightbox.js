debb49eb13cc0eff0ced61fe0cfa9cb612cea5b4
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-09-17 09:32:44 +0300

    End of homeWork 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}" data-caption="${description}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    
+    }).join('');
+}
+// console.log(markup);
+imageConteiner.addEventListener('click', onClickImage);
+const lightbox = new SimpleLightbox('.gallery a', {
+    // captions: true,
+    // // captionSelector: 'img',
+    // // captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250
+});
+// SimpleLightbox.default = {
+//     showCaption: true, captionAttribute: 'alt',
+// }
+function onClickImage(evt) {
+    evt.preventDefault()
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    const clickImg = evt.target.dataset.source
+    
+
+}
